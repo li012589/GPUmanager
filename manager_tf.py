a85b9eb865270f373eb4d4c35cb9567fbb52312d
Li SH <li012589li@outlook.com>
2017-11-06 17:22:32 +0800

    updae to use new manager.py

--- manager_tf.py
+++ manager_tf.py
@@ -1,23 +1,23 @@
-from  manager import GPUmanager
+from  manager import GPUManagerTemplate
 
 import tensorflow as tf
 
-class tfGPUmanager(GPUmanager):
-    def __init__(self,qargs=[],sessionargs=None,initSession=True):
-        super(tfGPUmanager,self).__init__(qargs):
+class tfGPUmanager(GPUManagerTemplate):
+    def __init__(self,qargs=[],sessionargs=None,initSession=None):
+        super(tfGPUmanager,self).__init__(qargs)
         if initSession is None:
-                if sessionargs is None:
-                    print("Start session using default args")
-                    sessionargs = tf.ConfigProto()
-                else:
-                    print("Start session using provided args")
-                config.allow_soft_placement=True
-                config.gpu_options.allow_growth=True
-                self.sess = tf.Session(config=sessionargs)
+            if sessionargs is None:
+                print("Start session using default args")
+                sessionargs = tf.ConfigProto()
             else:
-                print("Using provided session")
-                self.sess = initSession
-    def choice(self,mode=0,slience=False):
-        index = self._auto_choice(mode,slience)
+                print("Start session using provided args")
+            sessionargs.allow_soft_placement=True
+            sessionargs.gpu_options.allow_growth=True
+            self.sess = tf.Session(config=sessionargs)
+        else:
+            print("Using provided session")
+            self.sess = initSession
+    def choice(self,mode=0,slience=False,excludeUsed=False):
+        index = self.give(mode,slience,excludeUsed)
         return tf.device('/gpu:{}'.format(index))
 
